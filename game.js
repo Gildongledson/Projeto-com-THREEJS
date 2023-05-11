e4cd5780689f03aa7eabe2a6f57265c315acc4a1
Gildongledson <86193837+Gildongledson@users.noreply.github.com>
2023-05-11 16:32:03 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,166 +1,293 @@
 //variaveis da cena
-var camera, scene, renderer, material, maior=0.1, menor= -0.1;
+var camera,
+  scene,
+  renderer,
+  material,
+  world,
+  timeStep = 1 / 60,
+  groundBody,
+  boxBody,
+  groundMesh,
+  boxMesh,
+  maior = 0.1,
+  menor = -0.1
+  colisaoMao = null, //guarda o objeto que está colidindo com a mão
 init();
 
 function init() {
-    // Renderizador.
-    renderer = new THREE.WebGLRenderer();
-    
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // Adicionando redenrizador na tela
-    document.body.appendChild(renderer.domElement);
+  // Renderizador.
+  renderer = new THREE.WebGLRenderer();
 
-    // Criando a camera.
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    
-    camera.position.z = 300;
-    camera.position.y = 50;
-    
-    // criando a cena.
-    scene = new THREE.Scene();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // Adicionando redenrizador na tela
+  document.body.appendChild(renderer.domElement);
 
-    // Criando o material
-    material = new THREE.MeshPhongMaterial();
+  // Criando a camera.
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
 
-    //criando a primeira peça.
-    var geometry = new THREE.BoxGeometry(20, 70, 20);
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    this.mesh = mesh;
+  camera.position.z = 300;
+  camera.position.y = 50;
 
-    mesh.position.x = 0;
-		mesh.position.y = 0;
-		mesh.position.z = 0;
+  // criando a cena.
+  scene = new THREE.Scene();
 
+  const groundGeo = new THREE.PlaneGeometry(260, 260);
+  const groudMath = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    //wireframe: true
+  });
 
-    
-    //criando a segunda peça
-    var geometryy = new THREE.BoxGeometry(80, 10, 10);
-    const meshh = new THREE.Mesh(geometryy, material);
-    scene.add(meshh);
-    this.meshh = meshh;
+  groundMesh = new THREE.Mesh(groundGeo, groudMath);
+  scene.add(groundMesh);
 
-    meshh.position.x = 40;
-		meshh.position.y = 0;
-		meshh.position.z = 0;
+  world = new CANNON.World();
 
-    //criando a terceira peça
-    var geomet= new THREE.BoxGeometry(10, 40, 10);
-    const braco03 = new THREE.Mesh(geomet, material);
-    scene.add(braco03);
-    this.braco03 = braco03;
+  groundBody = new CANNON.Body({
+    shape: new CANNON.Plane(),
+    // mass: 10,
+    type: CANNON.Body.STATIC,
+    position: new CANNON.Vec3(0, -35, 0)
+  });
 
-    braco03.position.x = 0;
-		braco03.position.y = -20;
-		braco03.position.z = 0;
+  world.addBody(groundBody);
+  groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 
-    //criando junta
-    const geometrry = new THREE.CylinderGeometry(10, 10, 10);
-    const junta01 = new THREE.Mesh( geometrry, material );
-    scene.add( junta01 );
-    this.junta01 = junta01;
+  // Criando o material
+  material = new THREE.MeshPhongMaterial();
+  this.materialCubo = new THREE.MeshPhongMaterial({ color: 0xff0000 });
 
-    junta01.position.x = 0;
-		junta01.position.y = 40;
-		junta01.position.z = 0;
+  //criando a primeira peça.
+  var geometry = new THREE.BoxGeometry(20, 70, 20);
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  this.mesh = mesh;
 
-    //criando junta
-    const junta02 = new THREE.Mesh( geometrry, material );
-    scene.add( junta02 );
-    this.junta02 = junta02;
+  mesh.position.x = 0;
+  mesh.position.y = 0;
+  mesh.position.z = 0;
 
-    junta02.position.x = 40;
-		junta02.position.y = 0;
-		junta02.position.z = 0;
+  //criando a segunda peça
+  var geometryy = new THREE.BoxGeometry(80, 10, 10);
+  const meshh = new THREE.Mesh(geometryy, material);
+  scene.add(meshh);
+  this.meshh = meshh;
 
-    //criando junta
-    const formaovo = new THREE.SphereGeometry(10, 10, 10);
-    const mao = new THREE.Mesh( formaovo, material );
-    scene.add( mao);
-    this.mao = mao;
+  meshh.position.x = 40;
+  meshh.position.y = 0;
+  meshh.position.z = 0;
 
-    mao.position.x = 0;
-    mao.position.y = -20;
-    mao.position.z = 0;
+  //criando a terceira peça
+  var geomet = new THREE.BoxGeometry(10, 40, 10);
+  const braco03 = new THREE.Mesh(geomet, material);
+  scene.add(braco03);
+  this.braco03 = braco03;
 
-    //criando a ligação
+  braco03.position.x = 0;
+  braco03.position.y = -20;
+  braco03.position.z = 0;
 
-    mesh.add(junta01);
-    junta01.add(meshh);
-    meshh.add(junta02);
-    junta02.add(braco03);
-    braco03.add(mao);
+  //criando junta
+  const geometrry = new THREE.CylinderGeometry(10, 10, 10);
+  const junta01 = new THREE.Mesh(geometrry, material);
+  scene.add(junta01);
+  this.junta01 = junta01;
 
- 
-    // Criando luz na cena.
-    var light = new THREE.AmbientLight(0x404040); // soft white light
-    scene.add(light);
+  junta01.position.x = 0;
+  junta01.position.y = 40;
+  junta01.position.z = 0;
 
-    //direcionando a luz na cena.
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(1, 1, 1).normalize();
-    scene.add(directionalLight);
+  //criando junta
+  const junta02 = new THREE.Mesh(geometrry, material);
+  scene.add(junta02);
+  this.junta02 = junta02;
 
-    // Adicionando o listener.
-    window.addEventListener('resize', onWindowResize, false);
-    
-    // Adicionando listener para o telcado
-    document.body.addEventListener('keydown', keyPressed, false);
+  junta02.position.x = 40;
+  junta02.position.y = 0;
+  junta02.position.z = 0;
 
+  //criando junta
+  const formaovo = new THREE.SphereGeometry(10, 10, 10);
+  const mao = new THREE.Mesh(formaovo, material);
+  scene.add(mao);
+  this.mao = mao;
 
-    
-    render();
-
-}
-    //funcoes do teclado
-function keyPressed(e){
+  mao.position.x = 0;
+  mao.position.y = -20;
+  mao.position.z = 0;
   
-  switch(e.key) {
-    case 'ArrowUp':
-    	junta01.rotateZ(0.1);
-    	break;
-    case 'ArrowDown':
-    	junta01.rotateZ(-0.1);
-      break;
-    case '1':
-      junta01.rotateY(-0.1);
-      break;
-    case '3':
-      junta01.rotateY(0.1);
-      break;
-    case '8':
-      junta02.rotateZ(0.1);
-      break;
-    case '2':
-      junta02.rotateZ(-0.1);
-      break;
-    case '4':
-      junta02.rotateY(-0.1);
-      break;
-    case '6':
-      junta02.rotateY(0.1);
-      break;
-    case 'ArrowLeft':
-      mesh.rotateY(-0.1);
-      break;
-    case 'ArrowRight':
-      mesh.rotateY(0.1);
-      break;
-    
-  }
-  e.preventDefault();
+  // Mostra a boundingBox da mão, da qual é derivada a área de colisão.
+  this.maoHelper = new THREE.BoxHelper(mao, 0x00ff00) 
+  scene.add(maoHelper)
+  
+  //A forma de colisão é calculada por meio de uma bounding box (caixa de colisão) 
+  //que será calculada separadamente da mesh
+  this.maoBB     = new THREE.Box3() //Forma de colisão. Cria um cubo usando THREE.Box3()
+  this.maoBB.setFromObject(mao) //Usa a posição e o tamanho da mão para atribuir à forma de colisão
+
+  //Criando objeto cubo que será pego pela mão
+  const formaCubo = new THREE.BoxGeometry(15, 15, 15);
+  const cubo = new THREE.Mesh(formaCubo, materialCubo);
+  scene.add(cubo);
+  this.cubo = cubo;
+
+
+  boxBody = new CANNON.Body({
+    mass: 1,
+    //A função CANNON.Box() recebe um vetor com as dimensões do cubo divididas por 2.
+    //Um cubo de (15, 15, 15) deve receber um vetor de 7.5, 7.5, 7.5.
+    shape: new CANNON.Box(new CANNON.Vec3(7.5, 7.5, 7.5)),
+    //Posição do cubo é contada a partir do centro, com valor y um pouco acima do chão.
+    position: new CANNON.Vec3(13, -8, 25)
+  });
+
+  world.addBody(boxBody);
+
+
+  cubo.position.x = 70;
+  cubo.position.y = -27.5;
+  cubo.position.z = 0;
+
+  //Criando a área de colisão do cubo
+  const cuboBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+  cuboBB.setFromObject(cubo);
+  this.cuboBB = cuboBB;
+
+  //criando a ligação
+  mesh.add(junta01);
+  junta01.add(meshh);
+  meshh.add(junta02);
+  junta02.add(braco03);
+  braco03.add(mao);
+
+  // Criando luz na cena.
+  var light = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(light);
+
+  //direcionando a luz na cena.
+  var directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1).normalize();
+  scene.add(directionalLight);
+
+  // Adicionando o listener.
+  window.addEventListener("resize", onWindowResize, false);
+
+  // Adicionando listener para o telcado
+  document.body.addEventListener("keydown", keyPressed, false);
+
+  //define a gravidade do mundo como -9.82 no eixo y
+  world.gravity.set(0, -9.82, 0)
+
   render();
 }
-
+//funcoes do teclado
+function keyPressed(e) {
+  switch (e.key) {
+    case "ArrowUp":
+      junta01.rotateZ(0.1);
+      break;
+    case "ArrowDown":
+      junta01.rotateZ(-0.1);
+      break;
+    case "1":
+      junta01.rotateY(-0.1);
+      break;
+    case "3":
+      junta01.rotateY(0.1);
+      break;
+    case "8":
+      junta02.rotateZ(0.1);
+      break;
+    case "2":
+      junta02.rotateZ(-0.1);
+      break;
+    case "4":
+      junta02.rotateY(-0.1);
+      break;
+    case "6":
+      junta02.rotateY(0.1);
+      break;
+    case "ArrowLeft":
+      mesh.rotateY(-0.1);
+      break;
+    case "ArrowRight":
+      mesh.rotateY(0.1);
+      break;
+    case "z":
+      operateHand()
+      break;
+  }
+  e.preventDefault();
+}
 
 //função rederizar
-function render(){
-	renderer.render(scene, camera);
+function render() {
+  world.step(timeStep);
+
+  groundMesh.position.copy(groundBody.position);
+  groundMesh.quaternion.copy(groundBody.quaternion);
+
+  cubo.position.copy(boxBody.position);
+  cubo.quaternion.copy(boxBody.quaternion);
+
+  renderer.render(scene, camera);
 }
+
+renderer.setAnimationLoop(render);
+
 //funçao redimencionar
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+
+update()
+
+//Gameloop da aplicação, roda continuamente os comandos enquanto a janela estiver aberta em uma taxa de aprox. 30 frames por segundo.
+function update(){
+  // Atualiza as posições das bounding boxes constantemente,
+  // assim as formas de colisão criadas sempre seguirão os objetos.
+  maoBB.copy(mao.geometry.boundingBox).applyMatrix4(mao.matrixWorld)
+  cuboBB.copy(cubo.geometry.boundingBox).applyMatrix4(cubo.matrixWorld)
+
+  //Verifica se a mão está colidindo com o cubo, 
+  //se estiver, muda a cor do material associado ao cubo para verde
+  if (maoBB.intersectsBox(cuboBB)) {
+    materialCubo.color.setHex(0x00ff00)
+    colisaoMao = cubo
+  } else {
+    materialCubo.color.setHex(0xff0000)
+    colisaoMao = null
+  }
+
+  //Atualiza a visualização da bounding box da mão
+  maoHelper.update()
+
+  //Render foi movida para a função update, 
+  render()
+  requestAnimationFrame(update) //esta função do javascript permite que a função update seja chamada toda vez que a página for atualizada.
+}
+
+//Função que liga o cubo à mão caso haja colisão e solta o cubo caso não haja
+function operateHand(){
+  if (!colisaoMao) return
+  
+  //Se o cubo não estiver ligado à mão, adiciona o cubo à mão, senão solta
+  if (mao.children.length == 0)
+  {
+    cubo.position.set(0, -10, 0) //modifica a posição relativa do cubo para que ele fique no lugar correto ao ser pego
+    mao.add(cubo) //adiciona o cubo à mão para que ele a siga.
+  } else {
+    mao.remove(cubo) //remove o cubo da mão
+    cubo.matrixWorld.decompose(cubo.position, cubo.quaternion, cubo.scale) //converte a posição do cubo em valores globais para que o cubo não saia do lugar quando largado.
+    scene.add(cubo) //adiciona o cubo de volta à cena
+    colisaoMao = null
+  }
 }
